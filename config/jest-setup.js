811eddd7263b2dd36aadacee1cad69b7d297fc6a
Cédric RICARD <ricard33@gmail.com>
2021-10-27 07:01:41 -0400

    Fix tests for react 17

--- config/jest-setup.js
+++ config/jest-setup.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime'
 const Enzyme = require('enzyme')
-const Adapter = require('enzyme-adapter-react-16')
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17')
 
 Enzyme.configure({ adapter: new Adapter() })
 
